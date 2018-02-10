287b14a151a0248fd00910c3509ee606ea07a280
Sethu <u5849435@anu.edu.au>
2018-02-10 23:52:44 +1100

    documentation

--- app/src/main/java/com/example/sethu/mobile_authentication/LoggedInActivity.java
+++ app/src/main/java/com/example/sethu/mobile_authentication/LoggedInActivity.java
@@ -19,18 +19,23 @@ public class LoggedInActivity extends AppCompatActivity {
 
         String user = (String) getIntent().getExtras().get("userObject");
         Boolean userStatus = (Boolean) getIntent().getExtras().get("userStatus");
-        //Toast.makeText(this, user, Toast.LENGTH_SHORT).show();
         TextView textView = (TextView) findViewById(R.id.textView2);
         String welcome = userStatus ? "Welcome New User(": "Welcome Back (";
         textView.setText(welcome+user+")");
     }
 
+    /**
+     * Logging out from the activity
+     */
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Disabling back button
+     */
     public void onBackPressed(){
 
     }
